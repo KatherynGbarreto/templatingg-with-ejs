34ff31558b8d1aff6a96a134e5bf17c6d722b2e8
KatherynGbarreto <114604431+KatherynGbarreto@users.noreply.github.com>
2023-03-06 21:02:48 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,64 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+vconst express = require('express');
+const app = express();
+const port = 3000;
+var data = require('./data/test.json'); //here's a json file of user data
+
+app.set('view engine', 'ejs');
+
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+
+
+app.get('/', (req, res) => {
+  var title = 'Home Page';
+  res.render('pages/index', { 'title': title });
+});
+
+app.get('/about', (req, res) => {
+  var title = 'About Page';
+  res.render('pages/about', { 'title': title });
+});
+
+//Our list page is the default
+//users index is our list page
+app.get('/users', function(req, res) {
+  var title = 'Users Page';
+  res.render('users/index', {
+    title: title,
+    users: data
+  });
+});
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+  var title = 'User Page';
+  var id = req.params.id;
+  res.render('users/view', {
+    title: title,
+    user: data[--id]
+  });
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+  console.log(data);
+});
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
